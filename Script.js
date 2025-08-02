6b6cf5d0949f2c89b83586d9d592b029596eed81
vanevila27 <66636851+vanevila27@users.noreply.github.com>
2025-08-02 15:17:54 -0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -3,7 +3,7 @@ window.onload = function() {
 const fechaBoda = new Date('2025-10-11T19:00:00'); // Fecha y hora de la boda
 const contador = document.getElementById('contador');
 console.log(contador);
-    
+
 setInterval(() => {
     const ahora = new Date();
     const diferencia = fechaBoda - ahora;
@@ -14,8 +14,7 @@ setInterval(() => {
 
     contador.innerHTML = dias + " días " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
 }, 1000);
-    }
-    
+}
 
 // Mapa
 const mapa = document.getElementById('mapa');
