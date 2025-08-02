e1f45bb70c943c894e810085970fd8f98e376dcc
vanevila27 <66636851+vanevila27@users.noreply.github.com>
2025-08-02 15:12:39 -0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,7 +1,9 @@
 // Cronómetro
+window.onload = function() {
 const fechaBoda = new Date('2025-10-11T19:00:00'); // Fecha y hora de la boda
 const contador = document.getElementById('contador');
-
+console.log(contador);
+    
 setInterval(() => {
     const ahora = new Date();
     const diferencia = fechaBoda - ahora;
@@ -12,7 +14,10 @@ setInterval(() => {
 
     contador.innerHTML = dias + " días " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
 }, 1000);
+    }
+    
 
 // Mapa
 const mapa = document.getElementById('mapa');
 // Puedes utilizar una API de mapas como Google Maps o Leaflet para mostrar el mapa
+
