88928917f3f184ab3deb844918dba7b222d07fa4
vanevila27 <66636851+vanevila27@users.noreply.github.com>
2025-08-12 00:26:34 -0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -4,10 +4,26 @@ const anterior = document.querySelector('.anterior');
 const siguiente = document.querySelector('.siguiente'); 
 
 let current = 0;
+
+
+if (fotos.length > 0) {
+    fotos[current].style.display = 'block';
+    anterior.addEventListener('click', () => {
+        fotos[current].style.display = 'none';
+        current = (current - 1 + fotos.length) % fotos.length;
+        fotos[current].style.display = 'block';
+    });
+    siguiente.addEventListener('click', () => {
+        fotos[current].style.display = 'none';
+        current = (current + 1) % fotos.length;
+        fotos[current].style.display = 'block';
+    });
+}
 // Mapa
 const mapa = document.getElementById('mapa');
 // Puedes utilizar una API de mapas como Google Maps o Leaflet para mostrar el mapa
 }
+
 
 
 
