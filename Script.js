591e6b90c790ea08dda74035cc82c81e4aefd651
vanevila27 <66636851+vanevila27@users.noreply.github.com>
2025-08-11 23:36:45 -0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -3,10 +3,12 @@ const imagenes = document.querySelectorAll('.fotos img');
 const anterior = document.querySelector('.anterior');
 const siguiente = document.querySelector('.siguiente'); 
 
+let current = 0;
 // Mapa
 const mapa = document.getElementById('mapa');
 // Puedes utilizar una API de mapas como Google Maps o Leaflet para mostrar el mapa
 }
+
 
 
 
